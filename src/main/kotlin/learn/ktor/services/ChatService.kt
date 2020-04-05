b277bdab53ac8a4837be4efbe842e88ea7ba65fd
Todd Bednarczyk <todd@tjnar.com>
2020-04-05 15:06:56 -0400

    Some chat service cleanup

--- src/main/kotlin/learn/ktor/services/ChatService.kt
+++ src/main/kotlin/learn/ktor/services/ChatService.kt
@@ -9,35 +9,35 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import learn.ktor.api.chat.Message
 import learn.ktor.data.entities.UserEntity
-import learn.ktor.data.models.Users
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
 import java.io.IOException
 import java.util.concurrent.ConcurrentHashMap
 
 @Singleton
-class ChatService @Inject constructor(private val objectMapper: ObjectMapper) {
+class ChatService @Inject constructor(private val objectMapper: ObjectMapper, private val userService: UserService) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     private val members = ConcurrentHashMap<String, WebSocketSession>()
 
-    suspend fun memberJoin(member: String, socket: WebSocketSession) {
-        members.putIfAbsent(member, socket)
-        logger.debug("Member \"$member\" joined")
+    suspend fun memberJoin(sessionId: String, socket: WebSocketSession) {
+        members.putIfAbsent(sessionId, socket)
+        logger.debug("Member \"$sessionId\" joined")
     }
 
-    suspend fun memberLeft(member: String, socket: WebSocketSession) {
-        members.remove(member)
+    suspend fun memberLeft(sessionId: String, socket: WebSocketSession) {
+        members.remove(sessionId)
         transaction {
-            UserEntity.find { Users.sessionKey eq member }.firstOrNull()?.delete()
+            userService.deleteBy(sessionId)
         }
 
-        logger.debug("Member \"$member\" left")
+        logger.debug("Member \"$sessionId\" left")
     }
 
     suspend fun receivedMessage(id: String, body: String) {
         when {
             body.startsWith(MESSAGE, true) -> sendMessage(id, body)
+            else -> logger.debug("Unknown command received")
         }
     }
 
@@ -46,19 +46,20 @@ class ChatService @Inject constructor(private val objectMapper: ObjectMapper) {
         withContext(Dispatchers.IO) {
             try {
                 val message = objectMapper.readValue(unparsedMessage, Message::class.java)
-                val userSessionKeys = transaction {
-                    UserEntity.find { Users.sessionKey eq id }.first().room.users.map {
+
+                transaction {
+                    userService.getUserBy(id, UserEntity::room)?.room?.users?.map {
                         it.sessionKey
                     }
+                }?.let { userSessionKeys ->
+                    broadcast(message.data, userSessionKeys)
                 }
-                broadcast(message.data, userSessionKeys)
             } catch (e: IOException) {
                 logger.error("Unable to parse message")
             } catch (e: NoSuchElementException) {
                 logger.error("Unable to find user")
             }
-        }
-
+        } ?: logger.warn("Unable to send message")
     }
 
     private suspend fun broadcast(message: String, userSessionKeys: List<String>) {
--- src/main/kotlin/learn/ktor/services/UserService.kt
+++ src/main/kotlin/learn/ktor/services/UserService.kt
@@ -3,13 +3,21 @@ package learn.ktor.services
 import learn.ktor.data.entities.RoomEntity
 import learn.ktor.data.entities.UserEntity
 import learn.ktor.data.models.Users
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.with
+import kotlin.reflect.KProperty1
 
 class UserService {
-    fun getUserBy(sessionKey: String) = UserEntity.find { Users.sessionKey eq sessionKey }.firstOrNull()
+    fun getUserBy(sessionKey: String, vararg relations: KProperty1<out Entity<*>, Any?>) =
+            UserEntity.find { Users.sessionKey eq sessionKey }.with(*relations).firstOrNull()
 
     fun createUser(name: String, sessionKey: String, roomEntity: RoomEntity) = UserEntity.new {
         this.name = name
         this.sessionKey = sessionKey
         this.room = roomEntity
+    }
+
+    fun deleteBy(sessionKey: String) {
+        UserEntity.find { Users.sessionKey eq sessionKey }.firstOrNull()?.delete()
     }
 }
